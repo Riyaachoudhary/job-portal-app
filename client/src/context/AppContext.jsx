98f61cc2cc8669360f2c130180702f64287ed5ac
Riyaachoudhary <rc1472137@email.com>
2026-05-26 01:01:17 +0530

    code

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -17,6 +17,8 @@ import { jobsData } from "../assets/assets";
 
     const [jobs, setJobs]= useState([])
 
+    const[showRecruiterLogin, setShowRecruiterLogin]= useState(false)
+
     //Function to fetch jobs
     const fetchJobs = async()=>{
         setJobs(jobsData)
@@ -29,7 +31,9 @@ import { jobsData } from "../assets/assets";
     const value = {
         setSearchFilter, searchFilter,
         isSearched, setIsSearched,
-        jobs, setJobs
+        jobs, setJobs,
+        showRecruiterLogin, setShowRecruiterLogin,
+        
     }
     
     return (<AppContext.Provider value={value}>
