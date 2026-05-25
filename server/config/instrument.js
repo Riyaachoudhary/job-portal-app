10752155624dc14f97370594215fd798ff6cc48d
Riyaachoudhary <rc1472137@email.com>
2026-05-26 02:07:08 +0530

    code day

---
+++ server/config/instrument.js
@@ -0,0 +1,27 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://602b72c7073d7b7a82ac7a1fb8c584d2@o4511452423847936.ingest.de.sentry.io/4511452433285201",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: 'trace',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
+// Profiling happens automatically after setting it up with `Sentry.init()`.
+// All spans (unless those discarded by sampling) will have profiling data attached to them.
+Sentry.startSpan({
+  name: "My Span",
+}, () => {
+  // The code executed here will be profiled
+});
---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js'
+
+
+//Intialize Express
+const app = express()
+
+// Connect to DB
+await connectDB()
+
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/', (req,res)=> res.send("API Working"))
+
+//Port
+const PORT =process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    
+})
