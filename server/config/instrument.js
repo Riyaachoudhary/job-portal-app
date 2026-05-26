8917aa3356eba1fa79d9f1d08481c29141d0ef2f
Riyaachoudhary <rc1472137@email.com>
2026-05-26 22:15:04 +0530

    First Commit

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -1,14 +1,15 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-const Sentry = require("@sentry/node");
-const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration }  from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "https://602b72c7073d7b7a82ac7a1fb8c584d2@o4511452423847936.ingest.de.sentry.io/4511452433285201",
   integrations: [
     nodeProfilingIntegration(),
+    Sentry.mongooseIntegration()
   ],
   // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set sampling rate for profiling - this is evaluated only once per SDK.init call
   profileSessionSampleRate: 1.0,
   // Trace lifecycle automatically enables profiling during active traces
