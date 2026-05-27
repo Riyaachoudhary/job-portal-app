8a76a04a690d03236b03b06e96297d34a0331f3c
Riyaachoudhary <rc1472137@email.com>
2026-05-27 20:07:41 +0530

    code

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -2,6 +2,7 @@ import Company from "../models/Company.js";
 import bcrypt from 'bcrypt'
 import { v2  as cloudinary} from 'cloudinary'
 import generateToken from "../utils/generateToken.js";
+import Job from "../models/Job.js";
 
 // Register a new company
 export const  registerCompany = async (req, res)=>{
@@ -87,11 +88,52 @@ export const loginCompany = async (req, res)=>{
 
 
 //get company data
-export const getCompanyData = async (req, res)=>{}
+export const getCompanyData = async (req, res)=>{
+
+
+    try {
+        
+        const company =req.company
+
+        res.json({success:true, company})
+
+    } catch (error) {
+        res.json({
+            success:false, message:error.message
+        })
+    }
+}
 
 
 //post a new job
-export const postJob = async (req, res) =>{}
+export const postJob = async (req, res) =>{
+
+    const {title, description, location, salary, level, category}= req.body
+
+    const companyId =req.company._id
+
+    try {
+
+        const newJob = new Job({
+            title,
+            description,
+            location,
+            salary,
+            companyId,
+            date : Date.now(),
+            level,
+            category
+        })
+
+        await newJob.save()
+
+        res.json({success:true,  newJob})
+
+    } catch (error) {
+        res.json({success:false, message: error.message})
+    }
+    
+}
 
 
 //get company job applicants
@@ -99,7 +141,22 @@ export const getCompanyJobApplicants = async(req, res)=>{}
 
 
 //get company posted jobs
-export const getCompanyPostedJobs = async(req,res)=>{}
+export const getCompanyPostedJobs = async(req,res)=>{
+
+    try {
+       
+        const companyId= req.company._id
+
+        const jobs = await Job.find({companyId})
+
+        //(TODO) adding no. of applicants info in data
+
+        res.json({success:true, jobsData: jobs})
+        
+    } catch (error) {
+        res.json ({success:false, message: error.message})
+    }
+}
 
 
 //change job application status
@@ -107,7 +164,27 @@ export const ChangeJobApplicationsStatus = async(req, res)=>{}
 
 
 //  change job visibility
-export const changeVisiblity = async (req, res)=>{}
+export const changeVisiblity = async (req, res)=>{
+    try {
+        
+        const {id} = res.body
+
+        const companyId = req.company._id
+
+        const job = await Job.findById(id)
+
+        if (companyId.toString()=== job.companyId.toString()){
+            job.visible = !job.visible
+        }
+
+        await job.save()
+
+        res.json({success:true, job})
+
+    } catch (error) {
+        res.json({success:false, message: error.message})
+    }
+}
 
 
 
--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { ChangeJobApplicationsStatus, changeVisiblity, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controllers/companyController.js'
 import upload from '../config/multer.js'
+import { protectCompany } from '../middleware/authMiddleware.js'
+import Company from "../models/Company.js";
 
 
 
@@ -16,27 +18,28 @@ router.post('/login', loginCompany)
 
 
 //get company data
-router.get('/company', getCompanyData)
+router.get('/company', protectCompany, getCompanyData)
+
 
 
 //post a job
-router.post('/post-job', postJob)
+router.post('/post-job', protectCompany, postJob)
 
 
 //get applicants data of company
-router.get('/applicants', getCompanyJobApplicants)
+router.get('/applicants',protectCompany, getCompanyJobApplicants)
 
 
 //get company job list
-router.get('/list-jobs', getCompanyPostedJobs)
+router.get('/list-jobs',protectCompany, getCompanyPostedJobs)
 
 
 //chnage applications status
-router.post('/change-status', ChangeJobApplicationsStatus)
+router.post('/change-status', protectCompany,  ChangeJobApplicationsStatus)
 
 
 //chnage applications visiblity
-router.post('/change-visiblity', changeVisiblity)
+router.post('/change-visiblity', protectCompany, changeVisiblity)
 
 
-export default router
+export default router;
